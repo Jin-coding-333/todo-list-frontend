7a5d8fc7c38d6631e231b022f623117fc2012c06
Jinhan_Bae <wlsgks333@naver.com>
2026-01-17 19:44:46 +0900

    chore: 필요 없는 파일 삭제

--- src/api/axiosInstance.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { BASE_URL, TENANT_ID } from "@/constants/api";
-
-export const axiosInstance = axios.create({
-  baseURL: `${BASE_URL}/${TENANT_ID}`,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
